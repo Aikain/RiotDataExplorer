4bbd7c9d54fb248ac1933940b85060c29d4c3531
Ville Nupponen <ville.nupponen@aika.in>
2024-09-26 13:29:32 +0300

    Fix backend tests

--- backend/src/test/kotlin/in/aika/riotdataexplorer/RiotDataExplorerApplicationTests.kt
+++ backend/src/test/kotlin/in/aika/riotdataexplorer/RiotDataExplorerApplicationTests.kt
@@ -2,10 +2,15 @@ package `in`.aika.riotdataexplorer
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.PropertySource
+import org.springframework.test.context.TestPropertySource
 
+@TestPropertySource(properties = arrayOf(
+    "RIOT_API_KEY = invalid-api-key",
+))
 @SpringBootTest
 class RiotDataExplorerApplicationTests {
 
-	@Test
-	fun contextLoads() {}
+    @Test
+    fun contextLoads() {}
 }
